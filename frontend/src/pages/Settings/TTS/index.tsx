9ad304368e7f959cc9782ea722df795bcd656bbc
sup12135 <dkstnqls1355@naver.com>
2026-05-11 19:45:32 +0900

    TTSSkeleton

--- frontend/src/pages/Settings/TTS/index.tsx
+++ frontend/src/pages/Settings/TTS/index.tsx
@@ -1,9 +1,13 @@
-//src/pages/Settings/TTS/index.tsx
+//src/pages/Settings/TTS/index.tsx (SL_SC_03)
 
-function TTS(){
-  return(
-    <h1>TTS :)</h1>
-  )
+import TTSSkeleton from "./TTSSkeleton";
+
+function Settings() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw', overflow: 'hidden' }}>
+      <TTSSkeleton />
+    </div>
+  );
 }
 
-export default TTS;
+export default Settings;
